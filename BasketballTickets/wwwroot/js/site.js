eb011992fe10559c35b3e4aac61c6b399248bfd9
NickyMateev <bgdunkers@gmail.com>
2019-01-25 00:43:19 +0200

    Make navbar shopping cart responsive

--- BasketballTickets/wwwroot/js/site.js
+++ BasketballTickets/wwwroot/js/site.js
@@ -3,19 +3,23 @@
 
 // Write your JavaScript code.
 
-function bookSeatBtn(event, ticketId, seatNo) {
-    addToCart(ticketId);
+$('.dropdown-menu').click(function (e) {
+    e.stopPropagation();
+});
+
+function bookSeatBtn(event, ticket) {
+    addToCartRequest(ticket);
     event.remove();
-    createBookedBtn(ticketId, seatNo);
+    createBookedBtn(ticket);
 }
 
-function unbookSeatBtn(event, ticketId, seatNo) {
-    removeFromCart(ticketId);
+function unbookSeatBtn(event, ticket) {
+    removeFromCartRequest(ticket.id);
     event.remove();
-    removeBookedBtn(ticketId, seatNo);
+    removeBookedBtn(ticket);
 }
 
-function createBookedBtn(ticketId, seatNo) {
+function createBookedBtn(ticket) {
     var bookedSeats = document.getElementById("booked-seats");
 
     var seatDiv = document.createElement("div");
@@ -23,41 +27,41 @@ function createBookedBtn(ticketId, seatNo) {
 
     var seatBtn = document.createElement("button");
     seatBtn.classList.add("btn", "btn-warning", "ticket-btn");
-    seatBtn.textContent = "Seat #" + seatNo;
+    seatBtn.textContent = "Seat #" + ticket.seatNo;
     seatBtn.onclick = function (e) {
-        unbookSeatBtn(e.toElement, ticketId, seatNo);
+        unbookSeatBtn(e.toElement, ticket);
     };
 
     seatDiv.appendChild(seatBtn);
     bookedSeats.appendChild(seatDiv);
 }
 
-function removeBookedBtn(ticketId, seatNo) {
+function removeBookedBtn(ticket) {
     var availableSeats = document.getElementById("available-seats");
 
     var seatBtn = document.createElement("button");
     seatBtn.classList.add("btn", "btn-info", "ticket-btn");
-    seatBtn.textContent = "Seat #" + seatNo;
+    seatBtn.textContent = "Seat #" + ticket.seatNo;
     seatBtn.onclick = function (e) {
-        bookSeatBtn(e.toElement, ticketId, seatNo);
+        bookSeatBtn(e.toElement, ticket);
     };
 
     availableSeats.appendChild(seatBtn);
 }
 
-function addToCart(ticketId) {
+function addToCartRequest(ticket) {
     $.ajax({
         type: "POST",
         url: "/ShoppingCart/Add",
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(ticketId),
+        data: JSON.stringify(ticket.id),
         success: function () {
-            addTicketToNavMenu(ticketId);
+            addTicketToNavMenu(ticket);
         }
     });
 }
 
-function removeFromCart(ticketId) {
+function removeFromCartRequest(ticketId) {
     $.ajax({
         type: "DELETE",
         url: "/ShoppingCart/Remove",
@@ -69,21 +73,60 @@ function removeFromCart(ticketId) {
     });
 }
 
-function addTicketToNavMenu(ticketId) {
+function addTicketToNavMenu(ticket) {
     var navbarTickets = document.getElementById("navbar-tickets");
 
     var ticketDiv = document.createElement("div");
-    ticketDiv.id = "nav-ticket-" + ticketId;
+    ticketDiv.id = "nav-ticket-" + ticket.id;
     ticketDiv.classList.add("row", "shopping-cart-row");
 
     var link = document.createElement("a");
     link.classList.add("dropdown-item");
     link.setAttribute("asp-controller", "Tickets");
     link.setAttribute("asp-action", "Book");
+    link.setAttribute("asp-route-gameId", ticket.id);
+
+    var homeTeamImg = document.createElement("img");
+    homeTeamImg.setAttribute("src", ticket.homeTeamImg.substr(1));
+    homeTeamImg.setAttribute("width", "20px");
+    homeTeamImg.setAttribute("height", "16px");
+    homeTeamImg.setAttribute("asp-append-version", true);
+
+    var textTag = document.createElement("text");
+    textTag.textContent = "\xa0vs\xa0";
+
+    var awayTeamImg = document.createElement("img");
+    awayTeamImg.setAttribute("src", ticket.awayTeamImg.substr(1));
+    awayTeamImg.setAttribute("width", "20px");
+    awayTeamImg.setAttribute("height", "16px");
+    awayTeamImg.setAttribute("asp-append-version", true);
+
+    var dateTextTag = document.createElement("text");
+    dateTextTag.textContent = "\xa0Nov 28\xa0";
+
+    var seatTextTag = document.createElement("text");
+    seatTextTag.textContent = "\xa0(Seat #" + ticket.seatNo + ")\xa0\xa0\xa0\xa0";
+
+    var removeLink = document.createElement("a");
+    removeLink.classList.add("btn", "btn-danger", "btn-xs")
+    removeLink.onclick = function () {
+        removeFromCartRequest(ticket.id);
+    }
+
+    var removeGlyphicon = document.createElement("i");
+    removeGlyphicon.classList.add("glyphicon", "glyphicon-remove")
+
+    removeLink.appendChild(removeGlyphicon);
+
+    link.appendChild(homeTeamImg);
+    link.appendChild(textTag);
+    link.appendChild(awayTeamImg);
+    link.appendChild(dateTextTag);
+    link.appendChild(seatTextTag);
+    link.appendChild(removeLink);
 
     ticketDiv.appendChild(link);
     navbarTickets.appendChild(ticketDiv);
-
 
     cartQuantity = document.getElementById("cartQuantity");
     cartQuantity.textContent = parseInt(cartQuantity.textContent, 10) + 1
